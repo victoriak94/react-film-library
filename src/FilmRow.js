fdf905c2bdd627914666353947230bf34c33906b
Mina Mikhail <mina@fightingtheboss.com>
2017-10-13 09:52:19 -0400

    Refactor FilmRow into functional component

--- src/FilmRow.js
+++ src/FilmRow.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import FilmPoster from './FilmPoster.js';
 import Fave from './Fave.js';
 
-class FilmRow extends Component {
-  render() {
-    return (
-      <div className="film-row" onClick={this.props.onDetailsClick}>
-        <FilmPoster film={this.props.film} />
-        <div className="film-summary">
-          <h1>{this.props.film.title}</h1>
-          <p>{new Date(this.props.film.release_date).getFullYear()}</p>
-        </div>
-
-        <Fave isFave={this.props.isFave} onFaveToggle={this.props.onFaveToggle} />
+function FilmRow(props) {
+  return (
+    <div className="film-row" onClick={props.onDetailsClick}>
+      <FilmPoster film={props.film} />
+      <div className="film-summary">
+        <h1>{props.film.title}</h1>
+        <p>{new Date(props.film.release_date).getFullYear()}</p>
       </div>
-    )
-  }
+
+      <Fave isFave={props.isFave} onFaveToggle={props.onFaveToggle} />
+    </div>
+  )
 }
 
 export default FilmRow;
