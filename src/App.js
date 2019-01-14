0629700bff8c7dc7b65623d22351dd731419cb99
victoriak94 <vkamila6@gmail.com>
2019-01-13 21:01:54 -0500

    Create FilmDetails and FilmListing components and .js files. Move elements from App component to their own respective components.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="film-library">
-        <div className="film-list">
-          <h1 className="section-title">FILMS</h1>
-        </div>
-
-        <div className="film-details">
-          <h1 className="section-title">DETAILS</h1>
-        </div>
+        <FilmListing tmdb={TMDB}/>
+        <FilmDetails tmdb={TMDB}/>
       </div>
     );
   }
---
+++ src/FilmDetails.js
@@ -0,0 +1,10 @@
+
+class FilmDetails extends Component {
+  render () {
+    return (
+      <div className="film-list">
+        <h1 className="section-title">FILMS</h1>
+      </div>
+    );
+  }
+}
