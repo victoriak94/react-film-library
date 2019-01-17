02c9e5d31a2e367a3e8517630a0c2cfe88092283
victoriak94 <vkamila6@gmail.com>
2019-01-16 20:23:05 -0500

    Add filter markups to FilmListing. Add onClick on filter divs that call handleFilterClick with filter 'all' or 'faves' as arguments

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -4,14 +4,32 @@ import FilmRow from './FilmRow.js';
 class FilmListing extends Component {
   render () {
 
+    handleFilterClick(filter) {
+      console.log(`Setting filter to ${filter}`);
+    }
+
     const filmRows = this.props.films.map((film) => {
       return <FilmRow film={film} key={film.id} />
     });
 
     return (
       <div className="film-list">
-        <h1 className="section-title">FILMS</h1>
-        <p>{ filmRows }</p>
+        <h1 className="section-title">
+          FILMS
+        </h1>
+
+        <div className="film-list-filters">
+          <div className="film-list-filter" onClick={() => this.handleFilterClick('all')}>
+            ALL
+            <span className="section-count">{this.props.films.length}</span>
+          </div>
+          <div className="film-list-filter" onClick={() => this.handleFilterClick('faves')}>
+            FAVES
+            <span className="section-count">0</span>
+          </div>
+        </div>
+
+        {filmRows}
       </div>
     );
   }
